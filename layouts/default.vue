2be6fb7fe8ff649d90b982fb83b97278e9995481
rahman <rahmannurhayadi@gmail.com>
2025-07-05 17:45:48 +0700

    feat(layout) : add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="font-inter">
+        <nav>
+            <NavBar />
+        </nav>
+        <main class="w-dvw border">
+            <slot name="content" />
+        </main>
+    </div>
+</template>
