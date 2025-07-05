f31a656df377e78ef23f5c3fe9bba201351fce66
rahman <rahmannurhayadi@gmail.com>
2025-07-05 17:46:09 +0700

    feat(pages) : add homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-        <h1>BAZZARIDE</h1>
-    </div>
+    <NuxtLayout>
+        <template #content>
+            <div>
+                <h1>Temukan Event & Bazar UMKM Terdekat!</h1>
+                <p>
+                    Satu tempat untuk temukan dan dukung UMKM lokal melalui event & bazar seru di kotamu. Temui produk-produk unik, kuliner khas, dan peluang kolaborasi – semuanya dalam satu platform!
+                </p>
+            </div>
+        </template>
+    </NuxtLayout>
 </template>
