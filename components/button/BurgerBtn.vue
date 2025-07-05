94c6277201d17f0f4baf4d70e824f0f684f982b0
rahman <rahmannurhayadi@gmail.com>
2025-07-05 17:45:22 +0700

    feat(component) : add burgerBtn

---
+++ components/button/BurgerBtn.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-col w-10 gap-y-1 cursor-pointer lg:hidden" @click="toggleMobileMenu">
+        <span class="block h-2 bg-black transition duration-500"
+            :class="openDropdown ? 'rotate-45 translate-y-2' : ''"></span>
+        <span class="block h-2 bg-black transition duration-500" :class="openDropdown ? 'scale-0' : ''"></span>
+        <span class="block h-2 bg-black transition duration-500"
+            :class="openDropdown ? '-translate-y-4 -rotate-45' : ''"></span>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  openDropdown: boolean
+}>()
+const emits = defineEmits<{ (e: 'update:openDropdown', value: boolean):void }>()
+
+const toggleMobileMenu = () => {
+   emits('update:openDropdown', !props.openDropdown)
+}
+
+</script>
