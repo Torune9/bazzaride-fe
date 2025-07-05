58d80b41890f0a6975b1ce2d4224f747a88b4b7b
rahman <rahmannurhayadi@gmail.com>
2025-07-05 11:40:39 +0700

    feat(pages) : add auth  page

--- app.vue
+++ app.vue
@@ -4,12 +4,6 @@ const store = useCounterStore();
 
 <template>
   <div>
-    <!-- <NuxtRouteAnnouncer />
-    <NuxtWelcome /> -->
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <h1 class="text-2xl font-bold">Counter: {{ store.count }}</h1>
-    <button @click="store.increment" class="btn btn-primary mt-2">
-      Tambah
-    </button>
+    <NuxtPage/>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>BAZZARIDE</h1>
+    </div>
+</template>
