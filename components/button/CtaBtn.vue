f642c50bb19aadd608f3873d5e666ba8ff09f64a
rahman <rahmannurhayadi@gmail.com>
2025-07-06 16:24:08 +0700

    feat(component) : add ctabtn

---
+++ components/button/CtaBtn.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="p-3 border max-w-56 cursor-pointer" :class="roundedBtnOpt">
+        <slot/>
+    </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    rounded? : string
+}>()
+
+const roundedBtnOpt = computed(()=>{
+    switch(props.rounded){
+        case 'full':
+            return 'rounded-full';
+        case 'xl':
+            return 'rounded-xl';
+        case 'lg':
+            return 'rounded-lg';
+        case 'sm':
+            return 'rounded-sm';
+        default: 
+             return 'rounded'
+    }
+})
+</script>
