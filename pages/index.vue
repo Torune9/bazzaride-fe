4eb30fc79f8452dffd398e8a23194086e09e8272
rahman <rahmannurhayadi@gmail.com>
2025-07-06 16:25:55 +0700

    fix(pages) : add herosection in index

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,7 @@
 <template>
     <NuxtLayout>
         <template #content>
-            <div>
-                <h1>Temukan Event & Bazar UMKM Terdekat!</h1>
-                <p>
-                    Satu tempat untuk temukan dan dukung UMKM lokal melalui event & bazar seru di kotamu. Temui produk-produk unik, kuliner khas, dan peluang kolaborasi – semuanya dalam satu platform!
-                </p>
-            </div>
+            <HeroSection/>
         </template>
     </NuxtLayout>
 </template>
