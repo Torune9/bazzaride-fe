875defdd8339da587cf96a9cf18759b6bc119e8c
rahman <rahmannurhayadi@gmail.com>
2025-07-06 16:24:37 +0700

    feat(component) : add setItm homepage comp

---
+++ components/utils/StatItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="text-2xl">{{ value }}</div>
+    <div class="text-sm text-gray-600">{{ label }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  value: string | number
+  label: string
+}>()
+</script>
