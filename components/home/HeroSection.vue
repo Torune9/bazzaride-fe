c4e0d2aad8916858e9d7aaa37f05bc411562a1c1
rahman <rahmannurhayadi@gmail.com>
2025-07-07 11:00:52 +0700

    style(component) : fix layout herosection

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -1,21 +1,29 @@
+<script setup lang="ts">
+</script>
+
 <template>
-    <div class="flex flex-col h-[500px] gap-y-10 justify-center p-4">
-        <div class="flex flex-col gap-y-3">
-            <h1 class="text-4xl">Temukan Event & Bazar UMKM Terdekat!</h1>
-            <p class="text-sm">
-                Satu tempat untuk temukan dan dukung UMKM lokal melalui event & bazar seru di kotamu. Temui
-                produk-produk
-                unik, kuliner khas, dan peluang kolaborasi – semuanya dalam satu platform!
-            </p>
-            <CtaBtn rounded="full">
-                Gabung Sekarang
-            </CtaBtn>
+    <section>
+        <div class="flex flex-row min-h-[500px] gap-y-10 justify-center items-center">
+            <div class="flex flex-col gap-y-4">
+                <TitleSection class="text-4xl lg:text-6xl">Temukan Event &<br> Bazar UMKM Terdekat!</TitleSection>
+                <p class="text-sm/relaxed font-medium md:text-lg">
+                    Satu tempat untuk temukan dan dukung UMKM lokal melalui event & bazar seru di kotamu. Temui
+                    produk-produk
+                    unik, kuliner khas, dan peluang kolaborasi – semuanya dalam satu platform!
+                </p>
+                <CtaBtn rounded="full">
+                    Gabung Sekarang
+                </CtaBtn>
+                <div class="grid grid-cols-2 sm:grid-cols-4 gap-4 mt-10">
+                    <StatItem value="110+" label="Event" />
+                    <StatItem value="200+" label="Anggota" />
+                    <StatItem value="10+" label="On air" />
+                    <StatItem value="400+" label="Event berhasil" />
+                </div>
+            </div>
+            <div class="max-lg:hidden w-full h-full flex justify-end items-center">
+                <div class="w-96 h-96 rounded-full bg-blue-500"></div>
+            </div>
         </div>
-        <div class="grid grid-cols-2 sm:grid-cols-4 gap-4">
-            <StatItem value="110+" label="Event" />
-            <StatItem value="200+" label="Anggota" />
-            <StatItem value="10+" label="On air" />
-            <StatItem value="400+" label="Event berhasil" />
-        </div>
-    </div>
+    </section>
 </template>
