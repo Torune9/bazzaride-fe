db36f25a44e23fb5a45c4eda76a579e54529152a
rahman <rahmannurhayadi@gmail.com>
2025-07-07 11:00:07 +0700

    feat(component) : add about section

---
+++ components/home/AboutSection.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <div class="flex flex-col gap-y-8">
+            <div class="flex flex-col gap-y-4 lg:items-center lg:flex-row">
+                <TitleSection class="text-4xl lg:text-5xl">Tentang Kami</TitleSection>
+                <p class="md:text-lg font-semibold w-full flex justify-end">
+                    Berbasis di kota Bandung, Indonesia,<br>
+                    Bazzaride platform digital UMKM.
+                </p>
+            </div>
+            <p class="text-sm/relaxed lg:text-lg/relaxed lg:w-3/5">
+                Kami hadir untuk mempermudah pelaku usaha dalam memperluas jangkauan dan memperkenalkan produk mereka
+                kepada masyarakat luas. Melalui website ini, pengunjung dapat dengan mudah menemukan informasi mengenai
+                event bazar UMKM, sekaligus pelaku usaha dapat melakukan pemesanan booth secara online dengan cepat dan
+                praktis.
+            </p>
+        </div>
+    </section>
+</template>
