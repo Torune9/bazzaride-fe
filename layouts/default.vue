b0424e5bbc8352b446500445da2adb451ea8b308
rahman <rahmannurhayadi@gmail.com>
2025-07-08 08:59:19 +0700

    style(pages) : add sticky in navbar and change color footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="font-inter">
-        <nav>
+        <nav class="sticky top-0 bg-white/30 backdrop-blur-sm shadow-xs">
             <NavBar />
         </nav>
-        <main class="border flex flex-col gap-y-8 p-4 box-border md:px-16">
+        <main class="flex flex-col gap-y-8 p-4 box-border md:px-16">
             <slot name="content" />
         </main>
-        <footer class="p-4 bg-blue-500 md:px-16">
+        <footer class="p-4 bg-yellow-retro md:px-16">
             <FooterSection/>
         </footer>
     </div>
