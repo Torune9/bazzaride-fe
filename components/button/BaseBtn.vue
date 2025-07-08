c3a9695f17c1202c4409a11799798e6fe0158b63
rahman <rahmannurhayadi@gmail.com>
2025-07-08 17:28:32 +0700

    feat(component) : add base btn & back btn

---
+++ components/button/BaseBtn.vue
@@ -0,0 +1,5 @@
+<template>
+    <button  class="w-full rounded-full p-4 cursor-pointer">
+        <slot/>
+    </button>
+</template>
---
+++ components/common/Back.vue
@@ -0,0 +1,7 @@
+<template>
+    <NuxtLink
+        class="flex items-center w-max p-1 px-2 rounded-full hover:bg-stone-700 hover:text-stone-50 transition-colors duration-300"
+        to="/">
+        <Icon name="uil:arrow-circle-left" style="font-size: 20px;" />Back
+    </NuxtLink>
+</template>
