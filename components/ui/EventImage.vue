0be1dbdd3264064e3644fab7e75ba1fb952abe52
rahman <rahmannurhayadi@gmail.com>
2025-07-09 11:27:00 +0700

    feat(component) : add eventimg component

---
+++ components/ui/EventImage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-full h-64">
+        <img :src="src" alt="event-image" class="w-full h-full object-cover">
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    src : string
+}>()
+</script>
