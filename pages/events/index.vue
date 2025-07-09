a7745dd13d3dddf4f69c22f57e14d818df1ea494
rahman <rahmannurhayadi@gmail.com>
2025-07-09 11:27:24 +0700

    feat(pages) : add event pages

---
+++ pages/events/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <NuxtLayout>
+        <template #content>
+            <TitleSection class="text-4xl">Daftar Event</TitleSection>
+            <div>
+                <h2 class="text-2xl">Event Terbaru</h2>
+                <h3 class="text-lg">100+ Umkm telah bergabung </h3>
+            </div>
+            <div class="flex flex-wrap gap-6">
+                <CardEvent :event="event" v-for="event in events"/>
+            </div>
+            <div>
+                <InputCustom label="Search" type="text" id="search" placeholder="search..." v-model="search"/>
+            </div>
+            <!-- Daftar Event -->
+            <div class="flex flex-wrap gap-6">
+                <CardEvent :event="event" v-for="event in events"/>
+            </div>
+        </template>
+    </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+const search = ref('')
+
+const events = [
+    {
+        name : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, vitae!',
+        date : '10 juni 2025',
+        quota : 100
+    },
+    {
+        name : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, vitae!',
+        date : '10 juni 2025',
+        quota : 100
+    },
+    {
+        name : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, vitae!',
+        date : '10 juni 2025',
+        quota : 100
+    },
+    {
+        name : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, vitae!',
+        date : '10 juni 2025',
+        quota : 100
+    },
+]
+</script>
