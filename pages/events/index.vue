18d291ee4a07063676305e24d766eb2674f0f367
rahman <rahmannurhayadi@gmail.com>
2025-07-10 10:34:13 +0700

    style(eventPage) : fix responsiveness

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,44 +1,63 @@
 <template>
+    <div class="grid gap-20 lg:grid-cols-[1fr_400px]">
+        <div class="space-y-2 lg:order-2 flex flex-col justify-center lg:space-y-6">
+            <h2 class="font-medium text-3xl sm:text-4xl lg:text-6xl/tight">
+                Event <br class="hidden lg:block"> Terbaru
+            </h2>
+            <h3 class="text-lg sm:text-2xl">
+                100+ UMKM telah bergabung
+            </h3>
+        </div>
+        <div class="grid gap-6 sm:grid-cols-2 lg:order-1">
+            <CardEvent :event="event" v-for="event in newEst" />
+        </div>
+    </div>
     <TitleSection class="text-4xl">Daftar Event</TitleSection>
     <div>
-        <h2 class="text-2xl">Event Terbaru</h2>
-        <h3 class="text-lg">100+ Umkm telah bergabung </h3>
-    </div>
-    <div class="flex flex-wrap gap-6">
-        <CardEvent :event="event" v-for="event in events"/>
-    </div>
-    <div>
-        <InputCustom label="Search" type="text" id="search" placeholder="search..." v-model="search"/>
+        <InputCustom label="Search Event" type="text" id="search" placeholder="search..." v-model="search" />
     </div>
     <!-- Daftar Event -->
-    <div class="flex flex-wrap gap-6">
-        <CardEvent :event="event" v-for="event in events"/>
+    <div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <CardEvent :event="event" v-for="event in events" />
     </div>
 </template>
 
 <script setup lang="ts">
-const search = ref('')
-
+const search = ref("");
 const events = [
     {
-        name : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, vitae!',
-        date : '10 juni 2025',
-        quota : 100
+        id: '1',
+        name:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, vitae!",
+        quota: 100,
+        category: 'food',
+        date: "10 juni 2025",
     },
     {
-        name : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, vitae!',
-        date : '10 juni 2025',
-        quota : 100
+        id: '2',
+        name:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, vitae!",
+        quota: 100,
+        category: 'food',
+        date: "10 juni 2025",
     },
     {
-        name : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, vitae!',
-        date : '10 juni 2025',
-        quota : 100
+        id: '3',
+        name:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, vitae!",
+        quota: 100,
+        category: 'food',
+        date: "10 juni 2025",
     },
     {
-        name : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, vitae!',
-        date : '10 juni 2025',
-        quota : 100
+        id: '4',
+        name:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, vitae!",
+        quota: 100,
+        category: 'food',
+        date: "10 juni 2025",
     },
-]
+];
+const newEst = events.slice(0, 2);
+
 </script>
