94d22b41fb654e8feb7e453c27df9e8d3ab5f01a
rahman <rahmannurhayadi@gmail.com>
2025-07-10 09:01:11 +0700

    fix(event) : delete nuxtlayout in index

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,23 +1,19 @@
 <template>
-    <NuxtLayout>
-        <template #content>
-            <TitleSection class="text-4xl">Daftar Event</TitleSection>
-            <div>
-                <h2 class="text-2xl">Event Terbaru</h2>
-                <h3 class="text-lg">100+ Umkm telah bergabung </h3>
-            </div>
-            <div class="flex flex-wrap gap-6">
-                <CardEvent :event="event" v-for="event in events"/>
-            </div>
-            <div>
-                <InputCustom label="Search" type="text" id="search" placeholder="search..." v-model="search"/>
-            </div>
-            <!-- Daftar Event -->
-            <div class="flex flex-wrap gap-6">
-                <CardEvent :event="event" v-for="event in events"/>
-            </div>
-        </template>
-    </NuxtLayout>
+    <TitleSection class="text-4xl">Daftar Event</TitleSection>
+    <div>
+        <h2 class="text-2xl">Event Terbaru</h2>
+        <h3 class="text-lg">100+ Umkm telah bergabung </h3>
+    </div>
+    <div class="flex flex-wrap gap-6">
+        <CardEvent :event="event" v-for="event in events"/>
+    </div>
+    <div>
+        <InputCustom label="Search" type="text" id="search" placeholder="search..." v-model="search"/>
+    </div>
+    <!-- Daftar Event -->
+    <div class="flex flex-wrap gap-6">
+        <CardEvent :event="event" v-for="event in events"/>
+    </div>
 </template>
 
 <script setup lang="ts">
