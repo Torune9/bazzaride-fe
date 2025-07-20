c018b6c35dacdf1febfb7916d213bb4dbc6b9196
rahman <rahmannurhayadi@gmail.com>
2025-07-20 16:40:09 +0700

    feat (pages) : add card participant

---
+++ pages/participants/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <TitleSection class="text-4xl">
+        Participants
+    </TitleSection>
+    <Wrapper class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+       <Card/>
+       <Card/>
+       <Card/>
+    </Wrapper>
+</template>
