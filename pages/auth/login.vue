a2bc37247d81a9763612dd22f15d9b055dad3742
rahman <rahmannurhayadi@gmail.com>
2025-07-28 08:59:24 +0700

    feat(loginPage) : implement zod input error

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,8 +1,9 @@
 <template>
     <Container class="px-4 flex items-center justify-center lg:px-8 gap-x-10">
-        <div class="w-full h-screen max-lg:hidden flex flex-col gap-y-4 justify-center items-center bg-torqouise-retro rounded-tr-full rounded-tl-full">
+        <div
+            class="w-full h-screen max-lg:hidden flex flex-col gap-y-4 justify-center items-center bg-torqouise-retro rounded-tr-full rounded-tl-full">
             <NuxtImg src="/hero.png" alt="hero-image" class="w-3/4 rounded-full bg-white" />
-            
+
         </div>
         <!-- Wrapper -->
         <Wrapper class="space-y-6 w-full sm:w-3/4 lg:w-1/2 shrink-0 lg:px-16">
@@ -18,28 +19,15 @@
             <!-- End Info -->
             <!-- Form -->
             <div class="space-y-4">
-                <form action="" class="space-y-4">
-                    <InputCustom
-                        label="Email"
-                        id="email"
-                        type="email"
-                        placeholder="Jhondoe@mail.com"
-                        v-model="payload.email"
-                        size-class="p-4 rounded-full"
-                    >
+                <form @submit.prevent="handleSubmit" class="space-y-4">
+                    <InputCustom label="Email" id="email" type="email" placeholder="Jhondoe@mail.com"
+                        v-model="form.email" size-class="p-4 rounded-full" :errorMessage="errors.email as string">
                     </InputCustom>
-                    <InputCustom
-                        label="Password"
-                        id="password"
-                        type="password"
-                        v-model="payload.password"
-                        size-class="p-4 rounded-full"
-                    >
+                    <InputCustom label="Password" id="password" type="password" v-model="form.password"
+                        size-class="p-4 rounded-full"  :errorMessage="errors.password as string">
                     </InputCustom>
                     <div class="space-y-2">
-                        <BaseBtn
-                            class="bg-yellow-retro border hover:bg-yellow-300 transition-colors duration-300"
-                        >
+                        <BaseBtn class="bg-yellow-retro border hover:bg-yellow-300 transition-colors duration-300">
                             Masuk
                         </BaseBtn>
                         <div class="flex flex-row text-sm justify-between">
@@ -57,27 +45,36 @@
                 </form>
                 <BaseBtn
                     class="bg-purple-retro text-white flex items-center justify-center gap-x-2 hover:bg-purple-800 duration-300 transition-colors"
-                    type="button"
-                >
-                    <Icon
-                        name="uil:google"
-                        style="color: white; font-size: 25px"
-                    />Login dengan Google
+                    type="button">
+                    <Icon name="uil:google" style="color: white; font-size: 25px" />Login dengan Google
                 </BaseBtn>
             </div>
             <!-- End Form -->
             <Back />
         </Wrapper>
+
         <!-- End Wrapper -->
     </Container>
 </template>
-<script setup lang="ts">
-definePageMeta({
-    layout: false,
-});
 
-const payload = reactive({
-    email: "",
-    password: "",
-});
+<script setup lang="ts">
+import { useZodForm } from '~/composables/useZodForm'
+import { UserLoginSchema } from '~/model/formSchema'
+
+const form = reactive({
+  email: '',
+  password: ''
+})
+
+const { errors, validate } = useZodForm(UserLoginSchema, form)
+
+const handleSubmit = () => {
+  const result = validate()
+  if (result.success) {
+    // Kirim ke API
+    console.log('Form Valid', result.data)
+  } else {
+    console.warn('Form tidak valid')
+  }
+}
 </script>
