6058ecf2fa905c1b548506e68d9e9854c64a7ca8
rahman <rahmannurhayadi@gmail.com>
2025-07-29 09:36:34 +0700

    style(backBtn) : fix icon

--- components/common/Back.vue
+++ components/common/Back.vue
@@ -2,6 +2,6 @@
     <NuxtLink
         class="flex items-center w-max p-1 px-2 rounded-full hover:bg-stone-700 hover:text-stone-50 transition-colors duration-300"
         to="/">
-        <Icon name="uil:arrow-circle-left" style="font-size: 20px;" />Back
+        <Icon name="heroicons:arrow-left-16-solid" style="font-size: 20px;" />Back
     </NuxtLink>
 </template>
