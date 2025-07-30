5f5c559ba6a8b2feda4fde84b455056c047bc733
rahman <rahmannurhayadi@gmail.com>
2025-07-30 10:18:05 +0700

    style(baseBtn) : add disable attr

--- components/button/BaseBtn.vue
+++ components/button/BaseBtn.vue
@@ -1,5 +1,21 @@
 <template>
-    <button  class="w-full rounded-full p-4 cursor-pointer">
+    <button class="w-full rounded-full p-4" :class="disabled ? 'cursor-default' : 'cursor-pointer'" ref="btn">
         <slot/>
     </button>
 </template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    disabled? : boolean
+}>()
+
+const btn = useTemplateRef('btn')
+
+watch(()=>props.disabled,()=>{
+    if (props.disabled) {
+        btn.value?.setAttribute('disabled','')
+    }else{
+        btn.value?.removeAttribute('disabled')
+    }
+})
+</script>
