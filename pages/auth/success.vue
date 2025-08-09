a852c854b463486dd37d8026ce16d02b80645a53
rahman <rahmannurhayadi@gmail.com>
2025-08-09 09:49:15 +0700

    fix(successPage) : fix redirect when success login

--- pages/auth/success.vue
+++ pages/auth/success.vue
@@ -6,28 +6,40 @@
 
 <script setup lang="ts">
 import { useToast } from 'vue-toastification'
+import { userStore } from '~/store/userStore'
 
 definePageMeta({ layout: false })
 
 const router = useRouter()
+
+const storeUser = userStore()
 
 const toast = useToast()
 const useHandleApi = UseHandleApi()
 
 const getUser = async () => {
     try {
-        const response :any = await useHandleApi.get('/user/me')
+        const response: any = await useHandleApi.get('/user/me')
+
         toast.success(response.message)
-        if (!response.roleId) {
-            setTimeout(()=>{
+        
+        storeUser.userLogin = response.data
+        storeUser.isLogged = true
+        
+        if (!response.data.roleId) {
+            setTimeout(() => {
                 router.push('/role')
-            },1500)
+            }, 1500)
+        }else{
+            setTimeout(() => {
+                router.push('/events')
+            }, 1500)
         }
     } catch (error) {
         toast.error(error)
-        setTimeout(()=>{
+        setTimeout(() => {
             router.push('/auth/login')
-        },1500)
+        }, 1500)
         console.error('Gagal ambil user:', error)
     }
 }
