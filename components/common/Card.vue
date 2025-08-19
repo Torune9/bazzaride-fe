dff37c1489b17962a6fc6f1d1b7b2b5a88a9a76a
rahman <rahmannurhayadi@gmail.com>
2025-08-19 09:59:24 +0700

    fix(card) : fix data

--- components/common/Card.vue
+++ components/common/Card.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="grid w-full gap-2 rounded-xl overflow-hidden shadow-lg">
         <div class="w-full h-40">
-            <img src="https://placehold.co/400x600" alt="profile-img" class="h-full w-full object-cover">
+            <img :src="data.image ? data.image : 'https://placehold.co/400x600'" alt="profile-img"
+                class="h-full w-full object-cover">
         </div>
         <div class="w-full space-y-4 p-4">
             <h1 class="text-2xl font-medium">
-                Cireng Bandung
+                {{ data.name }}
             </h1>
             <div class="flex flex-row">
                 <p class="line-clamp-1">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere magnam dolor numquam ut omnis
-                    quibusdam dignissimos quae vel quisquam vero!
+                    {{ data.description }}
                 </p>
                 <CtaBtn class="w-14 h-14 flex items-center justify-center shrink-0" rounded="full">
                     <Icon name="heroicons:arrow-right-16-solid" style="font-size: 24px;" class="-rotate-45" />
@@ -19,3 +19,9 @@
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+const props = defineProps < {
+    data: Object
+} > ()
+</script>
